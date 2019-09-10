36fbaa7085d2ee2cb6baf0b3667ad0108b2381fd
yulu <yulu.li@continental.com>
2019-09-10 10:53:28 +0800

    delete commont

--- common/cmd_imx8wdt.c
+++ common/cmd_imx8wdt.c
@@ -2,7 +2,7 @@
 #include <command.h>
 #include "../board/digi/ccimx8x/ccimx8x.h"
 
-#ifdef __U_BOOT__
+
 static int do_wdt(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 {
     printf("hello imx8 watchdog triger:%s\n", argv[1]);
@@ -22,4 +22,3 @@ static int do_wdt(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 }
 
 U_BOOT_CMD(wdt, 2, 1, do_wdt, "wdt start/stop\n", "arg1: start/stop\n");
-#endif
